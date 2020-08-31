1739d0d0f75ee4d987ba726747cf251c0bf59da8
shopclues <shopclues@puruc2879>
2020-08-31 16:01:20 +0530

    Added dependency in TESTNG_Feature.java

--- testannotation/src/main/java/testannotation/testngannotations.java
+++ testannotation/src/main/java/testannotation/testngannotations.java
@@ -31,6 +31,7 @@ System.out.println("this is the before test data");
 }
 
 /*priority is used to define the sequence of test cases */
+
 @Test(priority = 1)
 public void testdata() {
 	System.out.println("this is the test data with priority 0");
@@ -39,6 +40,25 @@ public void testdata() {
 
 @Test(priority = 2)
 public void testdata2() {
-	System.out.println("this is the test data");
+	System.out.println("this is the test data 2");
+}
+
+
+/*group is used to maintain the testcases in a group weather it is active or not */
+
+
+@Test(priority = 3,groups="TEST1")
+public void testdata3() {
+	System.out.println("this is the test data 3 with group TEST1");
+}
+
+@Test(priority = 4,groups="TEST2")
+public void testdata4() {
+	System.out.println("this is the test data 4 with group TEST2" );
+}
+
+@Test(priority = 5,groups="TEST2")
+public void testdata5() {
+	System.out.println("this is the test data 5 with group TEST2" );
 }
 }
